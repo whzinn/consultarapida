7f8fa905f96762654dff1757889d3680238bd335
nexo <65514701+whzinn@users.noreply.github.com>
2023-04-06 12:22:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def form():
     return render_template('busca.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    app.run(host="0.0.0.0",port=80,debug=True)
 
