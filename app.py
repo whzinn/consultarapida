cec15a24109496bfad3d24130fab347bbda890d0
nexo <65514701+whzinn@users.noreply.github.com>
2023-04-06 12:47:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def form():
 """
     return render_template('busca.html')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=80,debug=True)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
