1a4e5e782d120943d8e90339810002e303538c9b
nexo <65514701+whzinn@users.noreply.github.com>
2023-04-07 11:29:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import mp
 import os
 app = Flask(__name__)
 
+@app.route('/pagar', methods=['POST'])
+def pagar()
+    pix = request.form["pix"]
+    return f"""<script>window.location = "{pix}";</script>
+"""
+
 @app.route('/', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
@@ -11,8 +17,7 @@ def form():
         email = request.form['campo-email']
         pix = mp.pix(email, tipo_consulta, campo)
         # fazer algo com os dados recebidos
-        return f"""<script>window.location = "{pix}";</script>
-"""
+        return redirect(url_for('pagar', pix=pix))
     return render_template('busca.html')
 
 if __name__ == "__main__":
