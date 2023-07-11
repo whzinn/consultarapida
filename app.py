3703185afe7aa3c89cce6030df574fc556766912
nexo <65514701+whzinn@users.noreply.github.com>
2023-07-11 08:04:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ def aqui():
     if request.method == 'POST':
         campo = request.form['campo-texto']
         email = request.form['campo-email']
-        pix = mp.pix(email, campo)
+        #pix = mp.pix(email, campo)
         #pix = "https://www.mercadopago.com.br/checkout/v1/payment/redirect/043fb990-1619-4c98-9c47-9d9609ff5959/payment-option-form/?preference-id=422523501-2a076704-eab2-4679-8ac8-797650077d75&router-request-id=f4563d60-f88f-40cd-8a74-730b5e41a969&webview=true&source=link&p=47edabca0cceb46bc47aa8decfeeaf16#/"
         # fazer algo com os dados recebidos
+        return render_template('pagar.html')
         return f"""<script>window.location = "{pix}";</script>"""
 @app.route('/', methods=['GET', 'POST'])
 def form():
