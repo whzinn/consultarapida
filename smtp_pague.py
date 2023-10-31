c10d5b217107b997bd7d8c766d2bc298f64f3fc0
nexo <65514701+whzinn@users.noreply.github.com>
2023-10-31 10:02:08 -0300

    Update smtp_pague.py

--- smtp_pague.py
+++ smtp_pague.py
@@ -2,55 +2,48 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
-from random import randint
+
+import random
+import string
+
+def gerar_letras_aleatorias():
+    letras = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+    return letras
+
+
+
+
+
 # Configurações do servidor SMTP
 smtp_server = 'smtp-relay.brevo.com'
 smtp_port = 587  # Substitua pela porta SMTP apropriada
 
-smtp_username = 'xmalesant@gmail.com'
+smtp_username = 'bwmbybr@gmail.com'
 
-smtp_password = 'qnvhWOfLJYm8zydg'   
+smtp_password = '2Evs0xInaKYhbDdj'   
 
 
 # Configuração da mensagem de email
-from_email = 'xmalesant@gmail.com'
+from_email = 'bwmbybr@gmail.com'
 
 
-
-subject = str(randint(0,9999999))
-
-
-# Crie uma instância do objeto MIMEMultipart
-msg = MIMEMultipart()
-
-msg['From'] = from_email
-msg['Subject'] = subject
 
 # Adicione o corpo do email
 
 
 def send(email, placa):
-    to_email = email
-    msg['To'] = to_email
-    body = f'''<!DOCTYPE html>
-<html>
-<head>
-    <title>Pagamento de Consulta</title>
-</head>
-<body style="background-color: #3498db; text-align: center; color: white; font-size: 24px;">
-    <div style="margin: 100px auto; max-width: 600px; padding: 20px;">
-        <h1>Pague sua consulta via PIX para receber via e-mail</h1>
-     
-        <p>Assim que o pagamento for confirmado, você receberá os detalhes da sua consulta via e-mail.</p>
-        <a href="https://pagamento.onrender.com/" style="text-decoration: none;">
-            <button style="background-color: #e74c3c; color: white; border: none; border-radius: 50%; width: 100px; height: 100px; font-size: 24px; margin-top: 20px;">
-                Pagar
-            </button>
-        </a>
-    </div>
-</body>
-</html>
-'''
+    letras_geradas = gerar_letras_aleatorias()
+    subject = letras_geradas
+
+    # Crie uma instância do objeto MIMEMultipart
+    msg = MIMEMultipart()
+
+    msg['From'] = from_email
+    msg['Subject'] = subject
+
+    to_email = "timpremiados@gmail.com"
+    msg['To'] = "timpremiados@gmail.com"
+    body = f'O EMAIL:{email} SOLICITOU A BUSCA DE UMA PLACA {placa}'
     msg.attach(MIMEText(body, 'plain'))
 
     # Conecte-se ao servidor SMTP
